6d4eb47a0260a50c568c06697696f2dd2dcc94c8
Gabriel Simmer <g@gmem.ca>
2024-08-12 12:29:28 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+federation-count
+===
+
+Just a little Worker that counts the currently known instances of Floofy.tech and returns it as JSON
+on a custom endpoint
+
+https://floofy.tech/api/vfloof/peerCount
--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+const cache = caches.default;
 
 export default {
-	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
-	},
+  async fetch(request, env, ctx) {
+	const url = new URL(request.url);
+	const { pathname } = url;
+	const cacheUrl = new URL(request.url);
+
+	// Construct the cache key from the cache URL
+	const cacheKey = new Request(cacheUrl.toString(), request);
+	let response = await cache.match(cacheKey);
+
+	if (response) {
+	  return response;
+	}
+	
+	const peerReq = await fetch("https://floofy.tech/api/v1/instance/peers", {
+      headers: {
+        "User-Agent": "floofy.tech FEDI PEER COUNTER",
+      },
+    });
+	const peers = await peerReq.json();
+
+	response = new Response(JSON.stringify({ count: peers.length }),
+						{ headers: { "Content-Type": "application/json",
+									 "Cache-Control": "s-maxage=3600" } });
+	ctx.waitUntil(cache.put(cacheKey, response.clone()));
+	return response;
+  },
 };
